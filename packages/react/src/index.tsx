d86a205910824f3bbd53a6adfc5503e2f8c9bc1d
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-23 14:54:30 -0300

    Create react Button Component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,30 @@
 import { styled } from './styles'
+import Stitches from '@stitches/react'
 
-const Button = styled('button', {
-  fontFamily: '$defalt',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  padding: '$4',
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Test</Button>
-}
+export type ButtonProps = Stitches.VariantProps<typeof Button>
