0313b3d913c602aa7a6801ae09ce7ce06952fa54
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-24 12:14:12 -0300

    feat: Add argTypes to Text stories

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,8 +7,31 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, magnam architecto quos inventore aspernatur cumque molestiae unde harum. Corrupti quo dolorem atque eius amet eveniet? Cumque tempora ratione natus harum.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
