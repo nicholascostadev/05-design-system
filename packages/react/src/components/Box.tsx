b1ee0be15b47ecf40c24993219f6e537739fa849
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-24 10:16:17 -0300

    feat: Box Component

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,15 @@
+import { styled } from '../styles'
+import Stitches from '@stitches/react'
+import { ElementType } from 'react'
+
+export const Box = styled('button', {
+  padding: '$4',
+  borderRadius: '$md',
+  backgroundColor: '$gray800',
+  border: '1px solid $gray600',
+  width: '100%',
+})
+
+export type BoxProps = Stitches.ComponentProps<typeof Box> & {
+  as?: ElementType
+}
